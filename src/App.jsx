c18019d45409ce9dede3e56417ad414282a85409
unknown <raushanraj1205@gmail.com>
2024-09-16 18:40:11 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import pfp from "../img/figma-1.png";
-import html from "../img/html.png";
-import abtme from "../img/abtme.png";
-import mail from "../img/mail.png";
-import media from "../img/media.png";
+
 import './index.css'
 function App() {
   return (
